6f273d298df3d46b404756a12688776cfd1b659d
Prashant Mittal <60237691+PrashantMittal54@users.noreply.github.com>
2020-03-18 14:48:56 -0500

    Update Generalized Hailstorm.py

--- Generalized Hailstorm.py
+++ Generalized Hailstorm.py
@@ -1,4 +1,10 @@
-# Assignment 1, Question 2:
+# Problem Statement: Write a program to test generalized hailstone numbers where xn = axn−1 +b if xn−1 is odd and xn = xn−1/2
+# if xn−1 is even where a and b are positive integers. For each of the a, b pairs less than or equal to 10, find
+# whether or not the sequence seems to converge. Once you can do that, tell me how many different holding
+# patterns the sequence converges to. For instance, when a = 3 and b = 5 then 1 ! 8 ! 4 ! 2 ! 1 this
+# is holding pattern. However, if we start with a different number, say 5 we get a different holding pattern
+# 5 ! 20 ! 10 ! 5.
+
 # Generalized Hailstorm
 
 
